e8a656fdbbf8cf6cf7f912d2395affdfd62d76d4
Rebecca Taylor <bex@beccasaur.us>
2021-05-07 12:50:50 -0700

    Update to support set -u

--- example.spec.sh
+++ example.spec.sh
@@ -18,10 +18,10 @@ testTrap='[ -n "${FUNCNAME[*]}" ] && echo -e "\e[31;1mStacktrace:\e[0m"; for ((i
 for testFn in $( declare -pF | awk '{ print $3 }' | grep "^test\|^spec" | sort -R ); do
   output="$( trap "$testTrap" ERR; [ -z "${BEFORE_TEST+x}" ] && set -eE || eval "$BEFORE_TEST"; $testFn 2>&1 )"
   case $? in
-    0) echo -e "[\e[32mPASS\e[0m] $testFn"; [ "$VERBOSE" = true ] && [ -n "$output" ] && printf '  \e[39;1m%s\e[0m\n%s\n' Output: "$( echo -e "$output" | sed 's/^/    /' )" ;;
+    0) echo -e "[\e[32mPASS\e[0m] $testFn"; [ "${VERBOSE:-}" = true ] && [ -n "$output" ] && printf '  \e[39;1m%s\e[0m\n%s\n' Output: "$( echo -e "$output" | sed 's/^/    /' )" ;;
     *) echo -e "[\e[31mFAIL\e[0m] $testFn"; anyFailed=$(( anyFailed = anyFailed + 1 )); [ -n "$output" ] && printf '  \e[39;1m%s\e[0m\n%s\n' Output: "$( echo -e "$output" | sed 's/^/    /' )" ;;
   esac
-done; [ -n "$anyFailed" ] && { echo -e "\e[31;1m$anyFailed test(s) failed\e[0m" >&2; exit 1; } || echo -e "\e[32;1mTests passed\e[0m"
+done; [ -n "${anyFailed:-}" ] && { echo -e "\e[31;1m$anyFailed test(s) failed\e[0m" >&2; exit 1; } || echo -e "\e[32;1mTests passed\e[0m"
 
 # Alternatively, simply source runTests.sh
 # source runTests.sh
