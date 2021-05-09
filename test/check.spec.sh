0720d7053c6f847664db5464505573647221d48f
Rebecca Taylor <bex@beccasaur.us>
2021-05-08 22:18:11 -0700

    CHECK_COLOR=false 🎨

--- test/check.spec.sh
+++ test/check.spec.sh
@@ -1,14 +1,9 @@
 test.runsMainExampleOK() {
-  local result="$( ./check example.spec.sh 2>&1 )" && : || :
-  result="$( removeColorCodes "$result" )"
+  local result="$( CHECK_COLOR=false ./check example.spec.sh 2>&1 )" && : || :
   echo "$result"
   [[ "$result" = *"[PASS] test.shouldPass"* ]]
   [[ "$result" = *"[FAIL] test.shouldFail"* ]]
   [[ "$result" = *"example.spec.sh:10 test.shouldFail"* ]]
   [[ "$result" = *"(( 1 == 0 )) # <-- this fails so the test fails"* ]]
   [[ "$result" = *"1 Test(s) Failed"* ]]
-}
-
-removeColorCodes() {
-  echo "$1" | sed -E "s/\x1B\[(([0-9]{1,2})?(;)?([0-9]{1,2})?)?[m,K,H,f,J]//g"
 }
