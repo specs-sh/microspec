bd1185fd222682b39577d01f0224339d25ecdb52
Rebecca Taylor <bex@beccasaur.us>
2021-05-08 22:04:04 -0700

    sed -E (for Mac OS)

--- test/check.spec.sh
+++ test/check.spec.sh
@@ -9,5 +9,5 @@ test.runsMainExampleOK() {
 }
 
 removeColorCodes() {
-  echo "$1" | sed -r "s/\x1B\[(([0-9]{1,2})?(;)?([0-9]{1,2})?)?[m,K,H,f,J]//g"
+  echo "$1" | sed -E "s/\x1B\[(([0-9]{1,2})?(;)?([0-9]{1,2})?)?[m,K,H,f,J]//g"
 }
