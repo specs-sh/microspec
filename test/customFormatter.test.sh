626d1dba785a12529000eaa0872ede2a721e3759
Rebecca Taylor <bex@beccasaur.us>
2021-05-10 13:45:04 -0700

    🧫  microtest

--- test/customFormatter.test.sh
+++
@@ -1,9 +0,0 @@
-test.TAP.formatter.works() {
-  local result="$( ./check example.spec.sh --formatter formatters/TAP.sh 2>&1 )" && : || :
-  echo "$result"
-  # [[ "$result" = *"[PASS] test.shouldPass"* ]]
-  # [[ "$result" = *"[FAIL] test.shouldFail"* ]]
-  # [[ "$result" = *"example.spec.sh:13 test.shouldFail"* ]]
-  # [[ "$result" = *"(( 1 == 0 )) # <-- this fails so the test fails"* ]]
-  # [[ "$result" = *"1 Test(s) Failed"* ]]
-}
--- test/exampleRunsOK.test.sh
+++
@@ -1,9 +0,0 @@
-test.runsMainExampleOK() {
-  local result="$( CHECK_COLOR=false ./check example.spec.sh 2>&1 )" && : || :
-  echo "$result"
-  [[ "$result" = *"[PASS] test.shouldPass"* ]]
-  [[ "$result" = *"[FAIL] test.shouldFail"* ]]
-  [[ "$result" = *"example.spec.sh:13 test.shouldFail"* ]]
-  [[ "$result" = *"(( 1 == 0 )) # <-- this fails so the test fails"* ]]
-  [[ "$result" = *"1 Test(s) Failed"* ]]
-}
